6d15a3528c561d0bda0f97b0f386e5af40a42b38
bgdar <akunzero975@gmail.com>
2025-09-18 13:11:20 -0400

    first profjet todo app
    menambah header management

--- include/file-management.hpp
+++ include/file-management.hpp
@@ -1,7 +1,6 @@
 #ifndef FILE_MANAGEMENT_HPP
-#define  FILE_MANAGEMENT_HPP
+#define FILE_MANAGEMENT_HPP
 
-#include <array>
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
@@ -18,10 +17,10 @@ private:
   std::ifstream readData;
 
   const std::string pathDataSvg = "../assert/data.csv";
-  std::vector<std::string> header ;
+  std::vector<std::string> header;
 
-  void writeHeader(){
-    for( auto row : header){
+  void writeHeader() {
+    for (auto row : header) {
       this->writeData << row;
       this->writeData << std::endl;
     }
@@ -29,66 +28,60 @@ private:
   }
 
 public:
-  FileManagement(std::vector<std::string> header ){
+  FileManagement(std::vector<std::string> header) {
     this->header = header;
     // tulis header header utama nya
     this->writeHeader();
   }
   // destructor
-  ~FileManagement(){
+  ~FileManagement() {
     this->writeData.close();
     this->readData.close();
     std::cout << "sumber daya untuk membaca dan menulis di close \n";
   }
 
 public:
-
   /**
-   * @brif kealikan data vector per barisnya 
+   * @brif kealikan data vector per barisnya
    */
- std::vector<std::string> readDataCsv(){
-   this->readData.open(this->pathDataSvg); 
-   std::string row ;
-   std::vector<std::string> newData;
-   
-   // tambah 1 baris 
-   while (std::getline(this->readData,row)) {
+  std::vector<std::string> readDataCsv() {
+    this->readData.open(this->pathDataSvg);
+    std::string row;
+    std::vector<std::string> newData;
+
+    // tambah 1 baris
+    while (std::getline(this->readData, row)) {
       newData.push_back(row);
-   }
-   return newData;
- }
+    }
+    return newData;
+  }
 
+  /*
+   * @brif mabil 1 baris yg pertama sekali di temukan
+   */
+  std::vector<std::string> getOneLine(std::string word) {
 
- /*
-  * @brif mabil 1 baris yg pertama sekali di temukan 
-  */
- std::vector<std::string> getOneLine(std::string word){
+    std::vector<std::string> newdata;
+    this->readData.open(this->pathDataSvg);
+    std::string row;
+    while (std::getline(this->readData, row)) {
+      std::string items;
+      std::istringstream stringStream(row);
 
-   std::vector<std::string> newdata ;
-   this->readData.open(this->pathDataSvg);
-   std::string row ;
-   while (std::getline(this->readData,row)) {
-     std::string items;
-     std::istringstream stringStream(row);
-
-     // loop pada setipa barisnya untuk collomn (pisah dengan ",")
-     while (std::getline(stringStream,items,',')) {
-       if (word == items) {
-// tambahkan baris yg di dapat
-        newdata.push_back(row);
-       }
-     
-     }
-   
-   }
-   return  newdata;
-
-
- } 
+      // loop pada setipa barisnya untuk collomn (pisah dengan ",")
+      while (std::getline(stringStream, items, ',')) {
+        if (word == items) {
+          // tambahkan baris yg di dapat
+          newdata.push_back(row);
+        }
+      }
+    }
+    return newdata;
+  }
 
   /**
    * @brif : tulis data ke file csv
-   * @params : data 
+   * @params : data
    *
    * @example :
    *   auto data = {
@@ -96,27 +89,25 @@ public:
    *    {"asasa","asasa","asas"},
    *    ...
    *  }
-   * 
+   *
    */
-  void writeFileCSv(const std::vector<std::vector<std::string>> data){
-  
+  void writeFileCSv(const std::vector<std::vector<std::string>> data) {
+
     this->writeData.open(pathDataSvg);
-    
+
     if (this->writeData.is_open()) {
-      for (const auto &row : data ) {
-        for (const auto &value : row){
-        this->writeData << value;
+      for (const auto &row : data) {
+        for (const auto &value : row) {
+          this->writeData << value;
         }
         this->writeData << std::endl;
       }
-      
-    }else{
+
+    } else {
       std::cerr << " file gagal di buka";
       std::exit(EXIT_FAILURE);
     }
-
   }
-
 };
 
 #endif // !FILE_MANAGEMENT_HPP
